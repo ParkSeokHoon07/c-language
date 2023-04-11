fe4ae003837a61943ef836f7f064f9a3ab5bf289
박석훈 <bagseoghun@kakao.com>
2023-04-11 07:04:07 +0000

    s

---
+++ source/55.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int a = 3;
+	a = a + 1;
+	
+	if(a > 5) {
+		printf("%d\n", a);
+	printf("조건이 참일때, 실행됩니다. \n");
+	}else {
+		printf("%d\n", a);
+	printf("조건이 거짓일때, 실행됩니다. \n");
+	}
+	//printf("%d\n", a - 6);
+}
