5caa094880f91b70a0a6c0a94df837f92247e5b7
박석훈 <bagseoghun@kakao.com>
2023-04-18 07:28:47 +0000

    2542446

---
+++ source/58.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	printf("%d %d\n", 3==2, 3!=1);
+	printf("%d %d\n", 3>2, 3<=2);
+	printf("%d \n", (3>2) && (3<=2) );
+	printf("%d \n", (3>2) || (3<=2) );
+	printf("%d \n", !(3>2) );
+	
+	
+	
+	
+	
+	
+	return 0;
+}
